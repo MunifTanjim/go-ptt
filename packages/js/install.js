2030133343053719b56a2f32ef0427f46fc14129
Munif Tanjim <hello@muniftanjim.dev>
2025-11-22 16:58:06 +0600

    fix(js): bump binary version

--- packages/js/install.js
+++ packages/js/install.js
@@ -11,7 +11,7 @@ const { extract } = require("tar");
 const { promisify } = require("node:util");
 
 const PACKAGE_NAME = "ptt";
-const VERSION = "0.13.0";
+const VERSION = "0.13.1";
 const GITHUB_REPO = "MunifTanjim/go-ptt";
 
 function getPlatformInfo() {
